b19783f69fde404f82679988a67498300f9a7dd8
Francesco Izzi <francesco.izzi@gmail.com>
2014-08-01 13:16:18 +0200

    Fix style request for legend

--- maplite-client/src/main/java/org/geosdi/maplite/client/GeoSDIMapLiteUiBinder.java
+++ maplite-client/src/main/java/org/geosdi/maplite/client/GeoSDIMapLiteUiBinder.java
@@ -196,7 +196,7 @@ public class GeoSDIMapLiteUiBinder extends Composite {
                 imageURL.append(raster.getDataSource()).append(GET_LEGEND_REQUEST)
                         .append(layerName).append("&scale=").append(map.getScale())
                         .append("&service=WMS&style=")
-                        .append(raster.getStyles().get(0));
+                        .append(raster.getStyles().get(0).getStyleString());
 
                 final Image legendImage = new Image(imageURL.toString());
                 legendImage.setVisible(raster.isChecked());
